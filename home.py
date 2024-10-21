29bbba6f8e5d1c47dee2d7a18330891ee6f4848e
Lucas Felipe <166764792+LucasFelipe296@users.noreply.github.com>
2024-10-21 18:44:42 -0300

    Update Home = Adicionado Nome e Saldo.

--- home.py
+++ home.py
@@ -2,17 +2,20 @@ import tkinter as tk
 from janela import Janela
 
 class Home(Janela):
-    def __init__(self, nome, titulo, usuario):
+    def __init__(self, nome, titulo, usuario, saldo):
         super().__init__(nome, titulo)
         self.usuario = usuario
+        self.saldo = saldo
 
     def criarHome(self):
         self.criar_janela()
         self.frameCima(30,'#4169e1')
         lTitulo = tk.Label(self.frame_superior,text='Menu Principal',bg='#4169e1',font=('Arial 15'),foreground='white')
         lTitulo.pack(padx=20)
-        lNome = tk.Label(self.janela,text=f'Nome: {self.usuario}')
-        lNome.pack(padx=20)
+        lNome = tk.Label(self.janela,text=f'Nome: {self.usuario}',anchor="nw", width=80)
+        lNome.pack(padx=20, pady=20)
+        lSaldo = tk.Label(self.janela,text=f'Saldo: {self.saldo}',anchor="nw", width=80)
+        lSaldo.pack(padx=20, pady=20)
 
 
 
@@ -20,6 +23,6 @@ class Home(Janela):
 
 
 
-app = Home("Home","Menu Principal","Jandherson")
+app = Home("Home","Menu Principal","Jandherson",10.50)
 app.criarHome()
 
